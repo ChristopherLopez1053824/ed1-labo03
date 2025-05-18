c9ed8a66a5942b177034fa4ff6c8c8e88941495b
Christopher Lopez <conradoclv@gmail.com>
2025-05-18 16:25:10 -0600

    Funcionando Lab 03

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,39 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
 public class E01TopKFrequentElements {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    PriorityQueue<Integer> Maxheap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+    HashSet<Integer> set = new HashSet<>();
+    public void add(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) == null) {
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+        }
+    }
+
+    void set(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        int[] result = new int[k];
+        add(nums);
+        set(nums);
+        for (int value : set) {
+            Maxheap.add(value);
+        }
+        for (int i = 0; i < k; i++) {
+            result[i] = Maxheap.poll();
+        }
+        return result;
     }
 }
--- src/main/java/ed/lab/E02AutocompleteSystem.java
+++ src/main/java/ed/lab/E02AutocompleteSystem.java
@@ -1,13 +1,53 @@
 package ed.lab;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class E02AutocompleteSystem {
+    HashMap<String, Integer>[]map;
+    String current = "";
 
+    class  Frequency{
+        String word;
+        int frequency;
+        public Frequency(String word, int frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+    }
     public E02AutocompleteSystem(String[] sentences, int[] times) {
-
+        map = new HashMap[26];
+        for (int i = 0; i < 26; i++) {
+            map[i] = new HashMap<String, Integer>();
+        }
+        for (int i = 0; i < sentences.length; i++) {
+            map[sentences[i].charAt(0) - 'a'].put(sentences[i], times[i]);
+        }
     }
 
     public List<String> input(char c) {
-        return null;
+        List<String> result = new ArrayList<>();
+        if(c == '#') {
+            map[current.charAt(0) - 'a'].put(current, map[current.charAt(0) - 'a'].getOrDefault(current, 0) + 1);
+            current = "";
+        }
+        else {
+            current += c;
+            List<Frequency> list = new ArrayList<>();
+            for (String s : map[current.charAt(0) - 'a'].keySet()) {
+                if (s.indexOf(current) == 0) {
+                    list.add(new Frequency(s, map[current.charAt(0) - 'a'].get(s)));
+                }
+            }
+            Collections.sort(list, (a, b) -> a.frequency == b.frequency ? a.word.compareTo(b.word) : b.frequency - a.frequency);
+            for (int i = 0; i < Math.min(3, list.size()); i++) {
+                result.add(list.get(i).word);
+            }
+        }
+        return result;
+
     }
 }
+
+
